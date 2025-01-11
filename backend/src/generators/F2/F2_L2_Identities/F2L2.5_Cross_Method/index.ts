62147d0289edccfd9f5b206101ccc937ee93b846
andy1388 <97766243+andy1388@users.noreply.github.com>
2025-01-12 01:11:46 +0800

    unwanted

--- backend/src/generators/F2/F2_L2_Identities/F2L2.5_Cross_Method/index.ts
+++
@@ -1,9 +0,0 @@
-import F2L2_5_Generator_Q1_F_MQ from './F2L2.5_Generator_Q1_F_MQ';
-import F2L2_5_Generator_Q2_N_MQ from './F2L2.5_Generator_Q2_N_MQ';
-import F2L2_5_Generator_Q3_N_MQ from './F2L2.5_Generator_Q3_N_MQ';
-
-export const generators = {
-    'F2L2.5_Generator_Q1_F_MQ': F2L2_5_Generator_Q1_F_MQ,
-    'F2L2.5_Generator_Q2_N_MQ': F2L2_5_Generator_Q2_N_MQ,
-    'F2L2.5_Generator_Q3_N_MQ': F2L2_5_Generator_Q3_N_MQ
-};
